85e3fa65efb18124c2b187790a75147881baa627
xBisharp <raduletuhoria@gmail.com>
2022-05-22 22:06:38 +0300

    added schelet pagini si rotunjit chestii la pagina de acasa

---
+++ lib/MainPages/races.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Races extends StatefulWidget {
+  const Races({Key? key}) : super(key: key);
+
+  @override
+  State<Races> createState() => RacesState();
+}
+
+class RacesState extends State<Races> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
