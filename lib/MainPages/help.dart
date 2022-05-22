9cc844de5295da7b7c3cbae061db50904abc4aab
Gandolh <alex.gusatu@mail.centruldecalcul.ro>
2022-05-23 01:34:26 +0300

    hai ca suntem zei

--- lib/MainPages/help.dart
+++ lib/MainPages/help.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 // l-as intreba pe barnie ce a facut pe pagina de our_team
 // dar probabil isi dadu shutdown pana acum...
 
@@ -20,25 +21,23 @@ class _HelpState extends State<Help> {
           memberData: MemberData(
               "Accesarea paginii acasa",
               "Se apasa pe iconita cu o casa din bara de navigatie de jos",
-              "https://www.clausweb.ro/intrebari-frecvente/wp-content/uploads/2015/06/notfound.png"),
+              'graphics/home.png'),
         ),
         MemberCard(
           memberData: MemberData(
               "Folosirea paginii Curse",
               "Se apasa pe iconita cu o masina din bara de navigatie de jos",
-              "https://www.clausweb.ro/intrebari-frecvente/wp-content/uploads/2015/06/notfound.png"),
+              'graphics/curse.png'),
         ),
         MemberCard(
           memberData: MemberData(
               "Accesarea paginii Echipa noastra",
               "Se apasa pe iconita cu o palarie de absolvent, pentru ca speram sa ajungem si noi in anul 2",
-              "https://www.clausweb.ro/intrebari-frecvente/wp-content/uploads/2015/06/notfound.png"),
+              'graphics/echipanoastra.png'),
         ),
         MemberCard(
-          memberData: MemberData(
-              "Accesarea paginii Ajutor",
-              "Deja esti pe ea ??????",
-              "https://www.clausweb.ro/intrebari-frecvente/wp-content/uploads/2015/06/notfound.png"),
+          memberData: MemberData("Accesarea paginii Ajutor",
+              "Deja esti pe ea ??????", 'graphics/ajutor.png'),
         )
       ],
     );
@@ -57,35 +56,47 @@ class MemberCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(
-                memberData.title,
-                style: titleTextStyle,
-                textAlign: TextAlign.start,
-              ),
-              Text(
-                memberData.description,
-                style: descriptionTextStyle,
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                child: Image.network(memberData.imageLink),
-              )
-            ]),
+            child: Neumorphic(
+                style: NeumorphicStyle(
+                    shape: NeumorphicShape.flat,
+                    boxShape:
+                        NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+                    depth: 8,
+                    lightSource: LightSource.topLeft,
+                    color: Colors.blue[300]),
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        memberData.title,
+                        style: titleTextStyle,
+                        textAlign: TextAlign.start,
+                      ),
+                      Text(
+                        memberData.description,
+                        style: descriptionTextStyle,
+                      ),
+                      Container(
+                          margin: const EdgeInsets.only(top: 10),
+                          child: Image.asset(memberData.imageLink),
+                          height: 400)
+                    ])),
             padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.all(50),
-            height: 350,
+            margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+            height: 550,
             width: 350,
             decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+                color: Colors.blue[300],
+                borderRadius: BorderRadius.circular(20)),
           ),
         ],
       ),
-      height: 500,
+      height: 600,
       width: 500,
       decoration: BoxDecoration(
         color: Colors.blue[300],
